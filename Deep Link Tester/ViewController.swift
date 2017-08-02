a9e526e2e2b28d62e1d8127b7d15d2c72203d499
Ethan Kreloff <ethan.kreloff@starz.com>
2017-08-02 12:27:19 -0600

    Initial Commit

---
+++ Deep Link Tester/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Deep Link Tester
+//
+//  Created by Ethan Kreloff on 8/2/17.
+//  Copyright © 2017 Ethan Kreloff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
